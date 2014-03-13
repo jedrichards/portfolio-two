1d0e333f79290c1f5436bceca196e757e5cc927c
jedrichards <jedrichards@gmail.com>
2014-03-13 01:05:58 +0000

    Middleware beginnings, html5 pushState app route

---
+++ server/middleware/logger.js
@@ -0,0 +1,9 @@
+module.exports = logger;
+
+function * logger (next) {
+	var start = Date.now();
+	yield next;
+	var ms = Date.now() - start;
+	// console.log(this);
+	console.log('%s %s %s (%sms)',this.method,this.url,this.status,ms);
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,22 @@
 var koa = require('koa');
-var koaStatic = require('koa-static');
-var app = koa();
+var static = require('koa-static');
+var route = require('koa-route');
+var send = require('koa-send');
 
-app.use(koaStatic(__dirname+'/../client/src'));
+var app = koa();
+app.name = require(__dirname+'/package.json').name;
+
+app.use(require("./middleware/logger"));
+app.use(static(__dirname+'/../client/src'));
+
+app.use(route.get('*',function * () {
+	yield send(this,'index.html',{root:__dirname+'/../client/src'});
+}));
+
+app.on('error',function (err) {
+	console.log(err);
+});
+
 app.listen(process.env.PORT,function () {
-	console.log("Server started on",process.env.PORT)
+	console.log('Server started on',process.env.PORT,'in',process.env.NODE_ENV);
 });
